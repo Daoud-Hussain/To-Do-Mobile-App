5b91d8cc8e8d5a23a3d2608a52e9d9419a86332b
Daoud-Hussain <daoudhussain302@gmail.com>
2023-10-23 15:37:37 +0500

    Updating and deleting TODOs

--- Todo.js
+++ Todo.js
@@ -4,11 +4,42 @@ import { View, TextInput, Button, Text, TouchableOpacity, FlatList, StyleSheet }
 const App = () => {
     const [text, setText] = useState('');
     const [items, setItems] = useState([]);
+    const [editId, setEditId] = useState(null); // To keep track of the item being edited
 
     const addItem = () => {
         if (text !== '') {
-            setItems([...items, { text, id: Date.now() }]);
+            if (editId !== null) {
+                // If an item is being edited, update it
+                const updatedItems = items.map((item) => {
+                    if (item.id === editId) {
+                        return { ...item, text };
+                    }
+                    return item;
+                });
+                setItems(updatedItems);
+                setEditId(null); // Clear the edit state
+            } else {
+                setItems([...items, { text, id: Date.now() }]);
+            }
             setText('');
+        }
+    };
+
+    const deleteItem = (id) => {
+        // Use filter to remove the item with the specified id
+        const updatedItems = items.filter(item => item.id !== id);
+        setItems(updatedItems);
+        if (editId === id) {
+            setEditId(null); // Clear the edit state if the edited item is deleted
+        }
+    };
+
+    const editItem = (id) => {
+        // Set the item's text in the input field for editing
+        const itemToEdit = items.find((item) => item.id === id);
+        if (itemToEdit) {
+            setText(itemToEdit.text);
+            setEditId(id);
         }
     };
 
@@ -24,22 +55,33 @@ const App = () => {
                     onChangeText={(text) => setText(text)}
                 />
                 <TouchableOpacity style={myStyles.appButtonContainer} onPress={addItem}>
-                    <Text style={myStyles.appButtonText}>Add Item</Text>
+                    <Text style={myStyles.appButtonText}>
+                        {editId !== null ? 'Update' : 'Add Item'}
+                    </Text>
                 </TouchableOpacity>
             </View>
 
-            <FlatList style = {myStyles.todos}
+            <FlatList
+                style={myStyles.todos}
                 data={items}
                 keyExtractor={(item) => item.id.toString()}
                 renderItem={({ item }) => (
                     <View style={myStyles.listItem}>
                         <Text>{item.text}</Text>
+                        <TouchableOpacity onPress={() => editItem(item.id)}>
+                            <Text style={{ color: 'blue' }}>Edit</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => deleteItem(item.id)}>
+                            <Text style={{ color: 'red' }}>Delete</Text>
+                        </TouchableOpacity>
                     </View>
                 )}
             />
         </View>
     );
 };
+
+
 
 const myStyles = StyleSheet.create({
     header: {
